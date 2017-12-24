05dce7ecb2fde71b8eb04b0d569c73f78547eb86
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-24 17:27:09 +0530

    creating the detail

--- src/components/artist.js
+++ src/components/artist.js
@@ -1,9 +1,38 @@
-import React,{Component} from 'react';
+import React,{Component} from 'react'
+
+import Header from './header'
+
+const REQ_URL="http://localhost:3004/artists"
 
 class Artist extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            artist:''
+        }
+    }
+
+    //fetch information from json
+    componentDidMount(){
+        fetch(`${REQ_URL}/${this.props.match.params.artistid}`,{
+            method:'GET'
+        }).then(response=>response.json())
+          .then(json=>{
+                this.setState({
+                    artist:json
+                })
+            })
+
+    }
+
     render(){
         return(
-            <div>Artist</div>
+            <div>
+                <Header/>
+
+            </div>
         )
     }    
 }
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Header=()=>{
+    return(
+        <header>
+            <Link to="/">MusicDB</Link>
+        </header>
+    )
+}
+
+export default Header;
